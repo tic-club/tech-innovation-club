40f6413aa9d6cb6c885e9e59faef1b66dcb018ac
Suresh Krishna <krishnapaulraj2004@gmail.com>
2023-10-12 17:46:27 +0530

    test

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>TEST</h1>
+      <h1>TEST Krish</h1>
       <Button onClick={logout}>Logout</Button>
     </main>
   );
